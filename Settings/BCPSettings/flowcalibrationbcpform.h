6ad5ef5c6de4cf7046863e30f700f02509a88231
QuantumMatter <kopala.david@gmail.com>
2020-06-12 13:27:26 -0600

    fixing weird button issues

--- Settings/BCPSettings/flowcalibrationbcpform.h
+++ Settings/BCPSettings/flowcalibrationbcpform.h
@@ -2,8 +2,13 @@
 #define FLOWCALIBRATIONBCPFORM_H
 
 #include <QWidget>
+#include <QMessageBox>
+#include <QDebug>
+#include <QtMath>
 
+#include "Communication/bcpserialhandler.h"
 #include "Settings/settingformshandler.h"
+#include "numberpadform.h"
 
 namespace Ui {
 class FlowCalibrationBCPForm;
@@ -17,15 +22,32 @@ public:
     explicit FlowCalibrationBCPForm(QWidget *parent = nullptr);
     ~FlowCalibrationBCPForm();
 
+    void NewCellFlowSlope(float s);
+    void NewOzoneFlowSlope(float s);
+
+    void UpdateLocalUI();
+    void GetNewSettings();
+
 private slots:
     void on_Left_clicked();
-
     void on_Right_clicked();
-
     void on_Home_clicked();
+    void on_CellSlopeMask_clicked();
+    void on_OzoneSlopeMask_clicked();
+    void on_Save_clicked();
 
 private:
     Ui::FlowCalibrationBCPForm *ui;
+
+    float cellFlowSlope = 0;
+    float ozoneFlowSlope = 0;
+
+    bool dontGrab = false;
+
+    bool OnExit();
+
+    void showEvent(QShowEvent * event);
+    void closeEvent(QCloseEvent * event);
 };
 
 #endif // FLOWCALIBRATIONBCPFORM_H
